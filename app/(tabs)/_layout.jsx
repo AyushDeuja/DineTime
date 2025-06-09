aa6af6494b9f31941f0b070dffd3a9a4921fe7b4
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-09 20:33:49 +0545

    layout created in tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,7 +1,13 @@
 import { Tabs } from "expo-router";
 
 const TabLayout = () => {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="home" options={{ title: "Home" }} />
+      <Tabs.Screen name="history" options={{ title: "History" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
+  );
 };
 
 export default TabLayout;
