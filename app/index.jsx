dbcb29af750c17c7f25bc03948e1b6626820f14c
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-10 11:11:04 +0545

    change in index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,7 @@
 import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.jsx to edit this screen.</Text>
-      <TouchableOpacity onPress={() => router.push("/home")}>
-        <Text className="text-xl">Change route</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <SafeAreaView></SafeAreaView>;
 }
