078aa4afe11cd8b62604b51ebeda98c4603e996c
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-11 18:32:57 +0545

    added logo and frame image

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,16 @@
 import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View>
+          <Image source={} style={{width:'300', height:'300'}}/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
