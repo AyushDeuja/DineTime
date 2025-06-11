4355d1226573fa44966007e97b6a1f77e70c4464
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-11 20:15:21 +0545

    signup button created

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,12 @@
 import { useRouter } from "expo-router";
-import { Image, ScrollView, StatusBar, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../assets/images/dinetimelogo.png";
 // const logo = require("../assets/images/dinetimelogo.jpg");
@@ -12,7 +19,13 @@ export default function Index() {
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="m-2 flex justify-center items-center">
           <Image source={logo} style={{ width: "300", height: "300" }} />
-          <View></View>
+          <View className="w-3/4">
+            <TouchableOpacity className="p-2 my-2 text-black rounded-lg bg-primary">
+              <Text className="text-base font-semibold text-center">
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
