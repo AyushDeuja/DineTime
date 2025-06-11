81040d30844d88edeae14d5b50dae060d9d9a85a
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-11 18:48:42 +0545

    colors setup in tailwind config

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,16 @@
 import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "../assets/images/dinetimelogo.jpg";
+// const logo = require("../assets/images/dinetimelogo.jpg");
 
 export default function Index() {
   const router = useRouter();
   return (
-    <SafeAreaView>
+    <SafeAreaView className={`bg-secondary`}>
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View>
-          <Image source={} style={{width:'300', height:'300'}}/>
+          <Image source={logo} style={{ width: "300", height: "300" }} />
         </View>
       </ScrollView>
     </SafeAreaView>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF8800",
+        secondary: "#1F2937",
+        lightText: "#1F2937",
+        darkText: "#E5E7EB",
+      },
+    },
   },
   plugins: [],
 };
