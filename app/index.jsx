bf1b55a76308c3b0593a1be6acc4c3b4f8e6e9d0
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-11 19:00:11 +0545

    removed background from images

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { Image, ScrollView, StatusBar, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import logo from "../assets/images/dinetimelogo.jpg";
+import logo from "../assets/images/dinetimelogo.png";
 // const logo = require("../assets/images/dinetimelogo.jpg");
 
 export default function Index() {
   const router = useRouter();
   return (
     <SafeAreaView className={`bg-secondary`}>
+      <StatusBar barStyle={"light-content"} className="bg-secondary" />
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View>
+        <View className="w-3/4">
           <Image source={logo} style={{ width: "300", height: "300" }} />
         </View>
       </ScrollView>
