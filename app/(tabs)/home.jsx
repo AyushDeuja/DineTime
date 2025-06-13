61a647a0fb1f1841c86003fa42baa438240dd14e
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-13 16:45:55 +0545

    colors added in home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,18 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "../../assets/Colors";
 
 const home = () => {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: Colors.SECONDARY }}>
+      <View className="flex items-center">
+        <View className="bg-[#5f5f5f]">
+          <View>
+            <Text></Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
