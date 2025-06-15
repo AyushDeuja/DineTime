bfe210dabc1fc30ee0e63d889a329f5f3c8cc06f
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-15 20:02:07 +0545

    restaurant component created

---
+++ app/restaurant/[restaurant].jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Restaurant = () => {
+  return (
+    <View>
+      <Text>[restaurant]</Text>
+    </View>
+  );
+};
+
+export default Restaurant;
