34ee1240f8af24c34bc665c24484473c5af1d3e6
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-16 15:46:07 +0545

    created three state variables

--- app/restaurant/[restaurant].jsx
+++ app/restaurant/[restaurant].jsx
@@ -1,10 +1,16 @@
 import { useLocalSearchParams } from "expo-router";
+import { useState } from "react";
 import { Platform, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "../../assets/Colors";
 
 const Restaurant = () => {
   const { restaurant } = useLocalSearchParams();
+
+  const [restaurantData, setRestaurantData] = useState({});
+  const [carousel, setCacousel] = useState({});
+  const [slots, setSlots] = useState({});
+
   return (
     <SafeAreaView
       style={[
