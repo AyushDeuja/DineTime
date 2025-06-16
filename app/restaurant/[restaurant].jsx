7eae504813691e7c28227730b767741b9c02ce6c
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-16 15:56:24 +0545

    restaurantData set

--- app/restaurant/[restaurant].jsx
+++ app/restaurant/[restaurant].jsx
@@ -1,8 +1,10 @@
 import { useLocalSearchParams } from "expo-router";
+import { collection, getDocs, query } from "firebase/firestore";
 import { useState } from "react";
 import { Platform, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "../../assets/Colors";
+import { db } from "../../config/firebaseConfig";
 
 const Restaurant = () => {
   const { restaurant } = useLocalSearchParams();
@@ -10,6 +12,26 @@ const Restaurant = () => {
   const [restaurantData, setRestaurantData] = useState({});
   const [carousel, setCacousel] = useState({});
   const [slots, setSlots] = useState({});
+
+  const getRestaurantData = async () => {
+    try {
+      const restaurantQuery = query(
+        collection(db, "restaurants"),
+        where("name", "==", restaurant)
+      );
+      const restaurantSnapshot = await getDocs(restaurantQuery);
+
+      if (restaurantSnapshot.empty) {
+        console.log("No matching restaurant found");
+        return;
+      }
+
+      for (const doc of restaurantSnapshot.docs) {
+        const restaurantData = doc.data();
+        setRestaurantData(restaurantData);
+      }
+    } catch (err) {}
+  };
 
   return (
     <SafeAreaView
